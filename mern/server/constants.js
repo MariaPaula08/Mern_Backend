003491883e89af8ec0961e3a38b8bf98fd439014
MariaPaula <mariapaulaembuspolanco@gmail.com>
2023-10-23 06:26:09 -0500

    mern

---
+++ mern/server/constants.js
@@ -0,0 +1,16 @@
+const DB_USER = "mariapaulaembuspolanco"
+const DB_PASSWORD = "3202472368"
+const DB_HOST = "cluster0.7uwhhoz.mongodb.net"
+
+const API_VERSION = "v1"
+const IP_SERVER = "localhost"
+const JWT_SECRET_KEY = "lsjabfkjdbsakjfher"
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SECRET_KEY
+}
